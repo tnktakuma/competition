8e3159db938776c5eeec5d2668b66b454e0e5328
田中卓磨 <tanakatakuma@takuMac.local>
2020-03-02 04:32:12 +0900

    first commit

---
+++ segment_tree.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class SegmentTree:
+    def __init__(self, a: List[int], identity: int):
+        self.a = a
+        self.n = len(a)
+        self.identity = identity
+        self.depth = (self.n - 1).bit_length
+        self.segment = 1 << self.depth - 1
+        self.tree = [identity] * (self.segment * 2 + 1)
+        self.tree[self.segment : self.n + self.segment] = a
+        for i in range(self.segment - 1, -1, -1):
+            self.tree[i] = self.f(self.tree[2*i+1], self.tree[2*i+2])
+
+    def update(self, i: int, x: int):
+        i += self.segment
+        self.tree[i] = x
+        while i:
+            i = (i - 1) // 2
+            self.tree[i] = self.f(self.tree[2*i+1], self.tree[2*i+2])
+
+    def query(self, l: int, r: int) -> int:
+        """Query at [l, r)
+
+        Args:
+            l (int): left
+            r (int): right
+
+        Returns:
+            int: result
+        """
+        if l >= r:
+            return self.identity
+        l += self.segment
+        r += self.segment - 1
+        res = self.identity
+        while l <= r:
+            if not l & 1:
+                res = self.f(self.tree[l], res)
+            if r & 1:
+                res = self.f(res, self.tree[r])
+            l = l // 2
+            r = (r - 2) // 2
+        return res
+
+    def f(self, x: int, y: int) -> int:
+        pass
---
+++ union_find_tree.py
@@ -0,0 +1,23 @@
+class UnionFindTree:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, i: int) -> int:
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def unite(self, i: int, j: int):
+        ri = self.find(i)
+        rj = self.find(j)
+        if ri != rj:
+            if self.rank[ri] < self.rank[rj]:
+                self.parent[ri] = rj
+        else:
+            self.parent[rj] = ri
+            if self.rank[ri] == self.rank[rj]:
+                self.rank[ri] += 1
+
+    def is_same(self, i: int, j: int) -> bool:
+        return self.find(i) == self.find(j)
